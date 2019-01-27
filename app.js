ece4d26f5d10640f4acc57612e8391f7b0984d48
Erica Naglik <erica2605@live.com>
2019-01-26 21:42:55 -0800

    users can create reviews

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
-const mongoose = requre('mongoose');
+const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// CREATE
+app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+        console.log(review);
+        res.redirect('/');
+    }).catch((err) => {
+        console.log(err.message);
+    })
+})
 
 const Review = mongoose.model('Review', {
     title: String,
+    description: String,
     movieTitle: String
 });
 
@@ -24,6 +39,11 @@ app.get('/', (req, res) => {
         .catch(err => {
             console.log(err);
         })
+})
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
 })
 
 
