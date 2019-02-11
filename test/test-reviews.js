1549a3ab890bb5800f5adc625dbe86793cd374e2
Erica Naglik <erica2605@live.com>
2019-02-10 19:48:16 -0800

    test delete added

--- test/test-reviews.js
+++ test/test-reviews.js
@@ -93,6 +93,20 @@ it('should update a SINGLE review on /reviews/<id> PUT', (done) => {
     });
   });
 
+// TEST DELETE
+it('should delete a SINGLE review on /reviews/<id> DELETE', (done) => {
+    var review = new Review(sampleReview);
+    review.save((err, data)  => {
+     chai.request(server)
+      .delete(`/reviews/${data._id}?_method=DELETE`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.html
+        done();
+      });
+    });
+  });
+});
 
 //tell mocha you want to test Reviews (this string is taco)
 describe('Reviews', ()  => {
